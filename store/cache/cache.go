91ccd5797cbd4dfbd3ecbce7a04d8aa6c388de05
mzack <marco.rivoli.nvh@gmail.com>
2021-08-25 20:43:32 +0200

    Replacing custom cache with gcache

--- store/cache/cache.go
+++ store/cache/cache.go
@@ -1,10 +1,22 @@
+// this is a wrapper around gcache - the eviction strategy is used to move data to disk
 package cache
 
 import (
-	"runtime"
-	"sync"
+	"fmt"
+	"math"
 	"time"
+
+	"github.com/bluele/gcache"
 )
+
+type Options struct {
+	Duration            time.Duration
+	Size                int
+	LRU                 bool
+	LFU                 bool
+	AdaptiveReplacement bool
+	OnEvicted           func(interface{}, interface{})
+}
 
 const (
 	NoExpiration      time.Duration = -1
@@ -15,9 +27,7 @@ type Cache interface {
 	SetWithExpiration(string, interface{}, time.Duration)
 	Set(string, interface{})
 	Get(string) (interface{}, bool)
-	Delete(string)
-	DeleteExpired()
-	OnEvicted(func(string, interface{}))
+	Delete(string) (interface{}, bool)
 	CloneItems() map[string]Item
 	Scan(func([]byte, []byte) error)
 	ItemCount() int
@@ -25,64 +35,36 @@ type Cache interface {
 
 type CacheMemory struct {
 	DefaultExpiration time.Duration
+	cache             gcache.Cache
 	Items             map[string]Item
-	mu                sync.RWMutex
 	onEvicted         func(string, interface{})
 	janitor           *janitor
 }
 
 func (c *CacheMemory) SetWithExpiration(k string, x interface{}, d time.Duration) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.set(k, x, d)
+	return c.set(k, x, d)
 }
 
-func (c *CacheMemory) set(k string, x interface{}, d time.Duration) {
-	var e int64
-	if d == DefaultExpiration {
-		d = c.DefaultExpiration
+func (c *CacheMemory) set(k string, x interface{}, d time.Duration) error {
+	if d <= 0 {
+		c.cache.Set(k, x)
 	}
-	if d > 0 {
-		e = time.Now().Add(d).UnixNano()
-	}
-	c.Items[k] = Item{
-		Object:     x,
-		Expiration: e,
-	}
+	return c.cache.SetWithExpire(k, x, d)
 }
 
-func (c *CacheMemory) Set(k string, x interface{}) {
-	c.SetWithExpiration(k, x, c.DefaultExpiration)
+func (c *CacheMemory) Set(k string, x interface{}) error {
+	return c.set(k, x, 0)
 }
 
 func (c *CacheMemory) Get(k string) (interface{}, bool) {
-	c.mu.RLock()
-	item, found := c.Items[k]
-	if !found {
-		c.mu.RUnlock()
+	if !c.cache.Has(k) {
 		return nil, false
 	}
-	if item.Expiration > 0 {
-		if time.Now().UnixNano() > item.Expiration {
-			c.mu.RUnlock()
-			return nil, false
-		}
+	value, err := c.cache.Get(k)
+	if err != nil {
+		return value, false
 	}
-	c.mu.RUnlock()
-	c.refresh(k)
-	return item.Object, true
-}
-
-func (c *CacheMemory) refresh(k string) bool {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	item, found := c.Items[k]
-	if !found {
-		return false
-	}
-	item.Expiration = time.Now().Add(c.DefaultExpiration).UnixNano()
-	return true
+	return value, true
 }
 
 func (c *CacheMemory) get(k string) (interface{}, bool) {
@@ -95,122 +77,72 @@ func (c *CacheMemory) get(k string) (interface{}, bool) {
 			return nil, false
 		}
 	}
-	c.refresh(k)
+
 	return item.Object, true
 }
 
-func (c *CacheMemory) Delete(k string) {
-	c.mu.Lock()
-	v, evicted := c.delete(k)
-	c.mu.Unlock()
-	if evicted {
-		c.onEvicted(k, v)
-	}
+func (c *CacheMemory) Delete(k string) (interface{}, bool) {
+	return c.delete(k)
 }
 
 func (c *CacheMemory) delete(k string) (interface{}, bool) {
-	if c.onEvicted != nil {
-		if v, found := c.Items[k]; found {
-			delete(c.Items, k)
-			return v.Object, true
-		}
+	v, err := c.cache.GetIFPresent(k)
+	if err != nil {
+		return nil, false
 	}
-	delete(c.Items, k)
+	if c.cache.Remove(k) {
+		c.onEvicted(k, v)
+		return v, true
+	}
+
 	return nil, false
 }
 
-// Delete all expired items from the cache.
-func (c *CacheMemory) DeleteExpired() {
-	var evictedItems []keyAndValue
-	now := time.Now().UnixNano()
-	c.mu.Lock()
-	for k, v := range c.Items {
-		// "Inlining" of expired
-		if v.Expiration > 0 && now > v.Expiration {
-			ov, evicted := c.delete(k)
-			if evicted {
-				evictedItems = append(evictedItems, keyAndValue{k, ov})
-			}
-		}
-	}
-	c.mu.Unlock()
-	for _, v := range evictedItems {
-		c.onEvicted(v.key, v.value)
-	}
-}
-
 func (c *CacheMemory) OnEvicted(f func(string, interface{})) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
 	c.onEvicted = f
 }
 
-func (c *CacheMemory) Scan(f func([]byte, []byte) error) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	for k, item := range c.Items {
-		f([]byte(k), item.Object.([]byte))
+func (c *CacheMemory) Scan(f func(interface{}, interface{}) error) {
+	for k, v := range c.cache.GetALL(true) {
+		f(k, v)
 	}
 }
 
-func (c *CacheMemory) CloneItems() map[string]Item {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	m := make(map[string]Item, len(c.Items))
-	now := time.Now().UnixNano()
-	for k, v := range c.Items {
-		// "Inlining" of Expired
-		if v.Expiration > 0 {
-			if now > v.Expiration {
-				continue
-			}
-		}
-		m[k] = v
+func (c *CacheMemory) CloneItems() map[string]interface{} {
+	items := make(map[string]interface{})
+	for k, v := range c.cache.GetALL(true) {
+		items[fmt.Sprint(k)] = v
 	}
-	return m
+	return items
 }
 
 func (c *CacheMemory) ItemCount() int {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	n := len(c.Items)
-
-	return n
+	return c.cache.Len(true)
 }
 
 func (c *CacheMemory) Empty() {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	c.Items = map[string]Item{}
+	c.cache.Purge()
 }
 
-func newCache(de time.Duration, m map[string]Item) *CacheMemory {
-	if de == 0 {
-		de = -1
+func New(options Options) *CacheMemory {
+	return NewFrom(options, nil)
+}
+
+func NewFrom(options Options, items map[string]Item) *CacheMemory {
+	maxSize := math.MaxInt32
+	if options.Size > 0 {
+		maxSize = options.Size
 	}
-	c := &CacheMemory{
-		DefaultExpiration: de,
-		Items:             m,
+	cache := gcache.New(maxSize)
+	if options.Duration > 0 {
+		cache = cache.Expiration(options.Duration)
 	}
-	return c
-}
-
-func newCacheWithJanitor(de time.Duration, ci time.Duration, m map[string]Item) *CacheMemory {
-	c := newCache(de, m)
-	if ci > 0 {
-		runJanitor(c, ci)
-		runtime.SetFinalizer(c, stopJanitor)
+	if options.AdaptiveReplacement {
+		cache = cache.ARC()
+	} else if options.LRU {
+		cache = cache.LRU()
+	} else if options.LFU {
+		cache = cache.LFU()
 	}
-	return c
-}
-
-func New(defaultExpiration, cleanupInterval time.Duration) *CacheMemory {
-	items := make(map[string]Item)
-	return newCacheWithJanitor(defaultExpiration, cleanupInterval, items)
-}
-
-func NewFrom(defaultExpiration, cleanupInterval time.Duration, items map[string]Item) *CacheMemory {
-	return newCacheWithJanitor(defaultExpiration, cleanupInterval, items)
+	return &CacheMemory{cache: cache.Build()}
 }
